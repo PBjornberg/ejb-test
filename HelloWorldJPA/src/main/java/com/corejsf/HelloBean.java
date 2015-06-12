61f4ff99232f80c7be34b3b29478b9eb539745c9
A U Thor <author@example.com>
2015-06-12 09:43:38 +0200

    JDBC Derby datasource

--- HelloWorldJPA/src/main/java/com/corejsf/HelloBean.java
+++ HelloWorldJPA/src/main/java/com/corejsf/HelloBean.java
@@ -1,25 +1,41 @@
 package com.corejsf;
 
+import java.util.ArrayList;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import my.group.dao.CustomerDAO;
+import my.group.entity.Customer;
+
 @ManagedBean(name = "hello")
 @SessionScoped
 public class HelloBean {
-    private String value = "Hello World!";
+	
+	private CustomerDAO customerDAO;
+//    private String value = "Hello World!";
+    String listString = "";
 
     @PostConstruct
     public void initialize() {
+    	customerDAO = new CustomerDAO();
         System.out.println(HelloBean.class.getSimpleName() + " was constructed");
     }
 
 
 	public String getValue() {
-		return value;
+		
+		for (Customer s : customerDAO.findAll())
+		{
+		    listString += s.getCustomername() + "\t";
+		}
+		
+		return listString;
+		//return value;
 	}
  
 	public void setValue(String value) {
-		this.value = value;
+		this.listString = value;
 	}
 }
