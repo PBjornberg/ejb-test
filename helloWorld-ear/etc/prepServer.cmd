22a491897864ed858649cdf0101ba6243bd8cf3c
Your Name <you@example.com>
2015-06-13 18:29:03 +0200

    Derby start script

--- helloWorld-ear/etc/prepServer.cmd
+++ helloWorld-ear/etc/prepServer.cmd
@@ -1,1 +1,3 @@
-C:\Apps\Oracle\wls1036\wlserver_10.3\common\bin\wlst.cmd serverConfig.py
+set CLASSPATH=C:\Apps\Oracle\wls1036_dev\wlserver\server\lib\weblogic.jar
+C:\Apps\Oracle\wls1036_dev\wlserver\common\bin\wlst.cmd serverConfig.py
+rem C:\Apps\Oracle\wls1036\wlserver_10.3\common\bin\wlst.cmd serverConfig.py
